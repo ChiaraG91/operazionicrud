690f6ea5e6cc734da6ddd5a4141b331fb19977e5
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-26 23:30:59 +0100

    Add files via upload

---
+++ operazionicrud/src/main/java/com/operazionicrud/operazionicrud/OperazionicrudApplication.java
@@ -0,0 +1,13 @@
+package com.operazionicrud.operazionicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperazionicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperazionicrudApplication.class, args);
+	}
+
+}
